dacf6f2fe8b71a9a349f2a610f3fb2718db8c0d8
Young <mcpjjh@gmail.com>
2017-01-26 00:56:22 +0000

    added validation and conditions for no characters or more than 140 characters + alert messages, won't post if conditions aren't met but will not clear message if not able to post either. minor css changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,70 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// var tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": {
-//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//     },
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
-// var data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-// var data = {};
-
 function renderTweets(data) {
   data.forEach(function(tweetData) {
     let $tweet = createTweetElement(tweetData);
@@ -89,7 +25,7 @@ function createTweetElement(tweetData) {
                 </header>
                 <p>${content}</p>
                 <footer>
-                 <div class=timestamp">
+                 <div class="timestamp">
                    <h5>${created_at}</h5>
                  </div>
                   <div class="controls">
@@ -113,45 +49,26 @@ function loadTweets() {
   })
 };
 
-
 $(function() {
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    $.ajax('/tweets', {
-      method: 'POST',
-      data: $(this).serialize()
-    })
-    .then(function() {
-      loadTweets()
-    })
-    .fail((error) => {
-      console.error(error)
-    })
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      if (document.getElementById("textInput").value == "") {
+        alert("You have nothing entered!");
+      } else if (document.getElementById("textInput").value.length > 140) {
+        alert("You have exceeded the character limit!");
+      } else {
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: $(this).serialize()
+      })
+      .then(function() {
+        loadTweets()
+      })
+      .fail((error) => {
+        console.error(error)
+      })
+    }
   })
   loadTweets()
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
